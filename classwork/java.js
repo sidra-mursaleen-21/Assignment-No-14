9d4cc2b1ed98f66094625840e967a89d5f0f1485
sidra mursaleen <sidramursaleen333@gmail.com>
2023-09-19 13:42:21 +0500

    add assignment

---
+++ classwork/java.js
@@ -0,0 +1,106 @@
+// ARRAYS AND LOOP
+
+// q no 3
+
+// for ( var i = 1 ; i <= 10 ; i++){
+//     document.write(i , "<br>");
+// }
+
+// q no 4
+
+// var tableNumber = +prompt("Enter a number to show it's multiplication table :");
+// var tableLenght = +prompt("Enter the lenght of multiplication table :");
+
+// for ( var i = 1 ; i <= tableLenght ; i++){
+//     document.write(tableNumber + " " + "* " + i + " " + "= " + tableNumber*i + "<br>");
+// }
+
+// q no 5
+
+// var fruits = ["apple" , "banana" , "mango" , "orange"  , "strawberry"];
+
+// for ( var i = 0 ; i < fruits.length ; i++){
+//     console.log(fruits[i]);
+// }
+
+// for ( var i = 0 ; i < fruits.length ; i++){
+//     console.log("Element at index " + i + " " + "is " + fruits[i]);
+// }
+
+// q no 6
+
+// document.write("COUNTING : <br>");
+
+// for ( var i = 1 ; i <= 15 ; i++){
+//     document.write(i + " , ");
+// }
+
+// document.write("<br>");
+
+// document.write("REVERSE COUNTING : <br>");
+
+// for (var i = 10; i >= 1; i--) {
+//   document.write(i + " , ");
+// }
+
+// document.write("<br>");
+
+// document.write("EVEN : <br>");
+
+// for (var i = 0; i <= 10; i++) {
+//   document.write(i*2 + " , ");
+// }
+
+// document.write("<br>");
+
+// document.write("ODD : <br>");
+
+// var num;
+
+// for (var i = 0; i <= 20; i++) {
+//     if (i % 2 != 0) {
+//       document.write(i + " , ")
+//     }
+// }
+
+// document.write("<br>");
+
+// document.write("SERIES : <br>");
+
+// for (var i = 1; i <= 10; i++) {
+//     document.write(i*2 + "K , ");
+// }
+
+// q no 8
+
+// var A = [24, 53, 78, 91, 12];
+// var max = A[0];
+
+// for ( var i = 0 ; i < A.length ; i++){
+//     if ( A[i] > max ){
+//         max = A[i];
+//     }
+// }
+
+// document.write("Array Items : " + A + "<br>");
+// document.write("The largest number is " + max);
+
+// q no 9
+
+// var A = [24, 53, 78, 91, 12];
+// var min = A[0];
+
+// for ( var i = 0 ; i < A.length ; i++){
+//     if ( A[i] < min ){
+//         min = A[i];
+//     }
+// }
+
+// document.write("Array Items : " + A + "<br>");
+// document.write("The smallest number is " + min);
+
+// q no 10
+
+// for(var i = 1 ; i <= 20 ; i++){
+//     document.write(i*5 + " , ");
+// }
---
+++ home assignment/java.js
@@ -0,0 +1,56 @@
+// q no 1
+
+// for(var i = 1 ; i <= 10 ; i++){
+//     document.write(i);
+// }
+
+// q no 2
+
+// for(var i = 1 ; i <= 12 ; i++){
+//    document.write(i);
+// }
+
+// q no 3
+
+// for ( var i = 0 ; i <= 4 ; i++){
+
+// }
+
+// q no 4
+
+// for ( var num = 0 ; num < 100 ; num++){
+//    document.write(num);
+// }
+
+// q no 5
+
+// for ( var i =  3 ; i > 0 ; i--){
+//    document.write(i);
+// }
+
+// q no 6
+
+// var fruits = ["apple" , "banana" , "mango" , "orange"  , "strawberry"];
+// var lenght = fruits.length;
+
+// q no 7
+
+// var flag = true ;
+
+// q no 8
+
+// var pets = ["dog" , "cat" , "parrat" , "birds"]
+
+// for ( var i = 0 ; i < pets.length ; i++ ){
+
+// }
+
+// q no 9
+
+// for ( var i = 0 ; i <= 10 ; i++){
+//    if (i == 1){
+//       alert(i);
+//    }
+//    break;
+// }
+ 
